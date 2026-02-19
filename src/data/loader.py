87d854c3d0ecd60f2a69db8f8889c07ae7b6f821
thatFarhan <fabdillahmu@gmail.com>
2026-02-19 17:31:00 +0700

    Changed: restructured the schedule management. Member details such as their name and discord uid are now inside anggota.json and jadwal_rawatib.json will reference the index of the list inside anggota.json

--- src/data/loader.py
+++ src/data/loader.py
@@ -6,20 +6,20 @@ def load_json(filename):
         return json.load(f)
     
 def save_presence(jadwal_hariini):
-    jadwal.presensi_petugas[str(date.today())] = jadwal_hariini
+    jadwal.presensi_rawatib[str(date.today())] = jadwal_hariini
 
-    with open('src/data/presensi_petugas.json', 'w') as file:
-        json.dump(jadwal.presensi_petugas, file, indent=2)
+    with open('src/data/presensi_rawatib.json', 'w') as file:
+        json.dump(jadwal.presensi_rawatib, file, indent=2)
 
-    jadwal.presensi_petugas = load_json("src/data/presensi_petugas.json")
-    jadwal.jadwal_hariini = load_json("src/data/presensi_petugas.json")[str(date.today())]
+    jadwal.presensi_rawatib = load_json("src/data/presensi_rawatib.json")
+    jadwal.jadwal_hariini = load_json("src/data/presensi_rawatib.json")[str(date.today())]
     
 class Jadwal():
     def __init__(self):
         self.jadwal_sholat_bulanini = None
         self.jadwal_rawatib = None
         self.anggota = None
-        self.presensi_petugas = None
+        self.presensi_rawatib = None
         self.jadwal_hariini = None
 
 jadwal = Jadwal()
@@ -27,4 +27,4 @@ jadwal = Jadwal()
 jadwal.jadwal_sholat_bulanini = load_json("src/data/jadwal_sholat/february.json")["data"]["jadwal"]
 jadwal.jadwal_rawatib = load_json("src/data/jadwal_rawatib.json")
 jadwal.anggota = load_json("src/data/anggota.json")
-jadwal.presensi_petugas = load_json("src/data/presensi_petugas.json")
+jadwal.presensi_rawatib = load_json("src/data/presensi_rawatib.json")
